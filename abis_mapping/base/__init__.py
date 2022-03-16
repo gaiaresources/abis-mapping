6ec0809f25b1182273360f2382a0f86fdbc5aa69
HaydenGaia <hayden.richards@gaiaresources.com.au>
2022-03-16 16:46:12 +0800

    Rearranged imports to remove circular import error

--- abis_mapping/base/__init__.py
+++ abis_mapping/base/__init__.py
@@ -1,7 +1,9 @@
 """Exports sub-package interface"""
 
 # Local
-from . import loader
 from . import mapper
 from . import metadata
 from . import types
+
+# Dynamically Load Templates
+from . import loader
