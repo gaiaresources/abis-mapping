2dfb81d358a7b7bb122d62e60153487fdb204c9a
HaydenGaia <hayden.richards@gaiaresources.com.au>
2022-03-22 14:09:40 +0800

    Re-define pandas types so they can be resolved.

--- abis_mapping/base/types.py
+++ abis_mapping/base/types.py
@@ -1,17 +1,21 @@
 """Provides Base Types for the Package"""
 
 
-# Third-Party
-import pandas._typing as pdt
-
 # Typing
-from typing import Union
+from os import PathLike
+from typing import IO, Union
 
+
+# Define Pandas Types
+# pandas._typing cannot be resolved properly, so re-defining them here
+FilePath = Union[str, PathLike[str]]
+ReadCsvBuffer = IO
 
 # Constants
 # See: https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html
+# See: https://github.com/pandas-dev/pandas/blob/v1.4.1/pandas/io/parsers/readers.py#L597
 CSVType = Union[
-    pdt.FilePath,
-    pdt.ReadCsvBuffer[bytes],
-    pdt.ReadCsvBuffer[str],
+    FilePath,
+    ReadCsvBuffer[bytes],
+    ReadCsvBuffer[str],
 ]
