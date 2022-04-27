6745d8384c8c5126c557d3b2d0ae2d002a493981
HaydenGaia <hayden.richards@gaiaresources.com.au>
2022-04-27 10:30:53 +0800

    Added functionality and tests for parsing `dd/mm/YYYY` date format

--- abis_mapping/utils/plugin.py
+++ abis_mapping/utils/plugin.py
@@ -2,6 +2,7 @@
 
 
 # Standard
+import contextlib
 import datetime
 
 # Third-Party
@@ -95,10 +96,15 @@ class TimestampType(frictionless.Type):
 
 # Helper Functions
 def parse_timestamp(raw: str) -> types.DateOrDatetime:
-    """Parses an ISO8601 string to a date or datetime with timezone
+    """Parses a string to a date or datetime with timezone
+
+    This function allows for the following formats:
+        (1): ISO86001 Date
+        (2): dd/mm/YYYY Date
+        (3): ISO86001 Datetime with Timezone
 
     Args:
-        raw (str): Raw ISO8601 string to be parsed
+        raw (str): Raw string to be parsed
 
     Returns:
         types.DateOrDatetime: Either a date or timezone aware datetime.
@@ -107,22 +113,23 @@ def parse_timestamp(raw: str) -> types.DateOrDatetime:
         ValueError: Raised if the string cannot be parsed as either a date or
             timezone aware datetime
     """
-    # Catch Errors
-    try:
-        # Parse as `date` first
-        timestamp = datetime.date.fromisoformat(raw)
+    # (1) Try Parse as ISO86001 Date
+    with contextlib.suppress(Exception):
+        return datetime.date.fromisoformat(raw)
 
-    except ValueError as exc:
-        # Timestamp is not a date, try `datetime`
+    # (2) Try Parse as `dd/mm/YYYY` Date
+    with contextlib.suppress(Exception):
+        return datetime.datetime.strptime(raw, "%d/%m/%Y").date()
+
+    # (3) Try Parse as ISO Datetime with Timezone
+    with contextlib.suppress(Exception):
         timestamp = datetime.datetime.fromisoformat(raw)
+        assert timestamp.tzinfo is not None
+        return timestamp
 
-        # Check for Timezone
-        if not timestamp.tzinfo:
-            # Re-Raise Error
-            raise ValueError("Datetime must include timezone") from exc
-
-    # Return
-    return timestamp
+    # Could not parse the string to a date or a datetime
+    # Raise a ValueError
+    raise ValueError(f"Could not parse '{raw}' as date or datetime with timezone")
 
 
 # Register Timestamp Plugin
