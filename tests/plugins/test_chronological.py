edcec7ebd88186fc792de46b8658ae5be3c0a96c
joecrowleygaia <joseph.crowley@gaiaresources.com.au>
2023-09-12 17:07:04 +0800

    added multiple date test scenarios to chronological order checks

--- tests/plugins/test_chronological.py
+++ tests/plugins/test_chronological.py
@@ -9,7 +9,7 @@ import pytest
 from abis_mapping import plugins
 
 
-@pytest.mark.parametrize("source, n_errors", [
+@pytest.mark.parametrize("source, field_names, n_errors", [
     ([
         # Missing
         {"name": "A", "start": "2022-09-11T15:15:15Z", "end": None},
@@ -21,7 +21,7 @@ from abis_mapping import plugins
 
         # Invalid - start 1 year after end
         {"name": "E", "start": "2023-09-11T15:15:15Z", "end": "2022-09-11T15:15:15Z"},
-    ], 1),
+    ], ["start", "end"], 1),
     ([
         # Missing
         {"name": "A", "start": "2022-09-11", "end": None},
@@ -33,7 +33,7 @@ from abis_mapping import plugins
 
         # Invalid - start 1 year after end
         {"name": "E", "start": "2023-09-11", "end": "2022-09-11"},
-    ], 1),
+    ], ["start", "end"], 1),
     ([
         # Missing
         {"name": "A", "start": "2022-09-11", "end": None},
@@ -45,9 +45,45 @@ from abis_mapping import plugins
 
         # Valid - start 1 year before end
         {"name": "E", "start": "2022-09-11", "end": "2023-09-11"},
-    ], 0)
+    ], ["start", "end"], 0),
+    ([
+        # Missing
+        {"name": "A", "start": "2022-09-11", "middle": "2022-10-11", "end": None},
+        {"name": "B", "start": None, "middle": None, "end": "2022-09-11"},
+        {"name": "C", "start": None, "middle": "2022-09-11", "end": None},
+
+        # Valid
+        {"name": "D", "start": "2022-09-11", "middle": "2023-05-11", "end": "2023-09-11"},
+
+        # Invalid - start 1 year after end
+        {"name": "E", "start": "2023-09-11", "middle":"2023-05-11", "end": "2022-09-11"},
+    ], ["start", "middle", "end"], 1),
+    ([
+        # Missing
+        {"name": "A", "start": "2022-09-11", "middle": "2022-10-11", "end": None},
+        {"name": "B", "start": None, "middle": None, "end": "2022-09-11"},
+        {"name": "C", "start": None, "middle": "2022-09-11", "end": None},
+
+        # Valid
+        {"name": "D", "start": "2022-09-11", "middle": "2023-05-11", "end": "2023-09-11"},
+
+        # Invalid - middle 1 year after end
+        {"name": "E", "start": "2022-09-11", "middle": "2024-05-11", "end": "2023-09-11"},
+    ], ["start", "middle", "end"], 1),
+    ([
+         # Missing
+         {"name": "A", "start": "2022-09-11", "middle": "2022-10-11", "end": None},
+         {"name": "B", "start": None, "middle": None, "end": "2022-09-11"},
+         {"name": "C", "start": None, "middle": "2022-09-11", "end": None},
+
+         # Valid
+         {"name": "D", "start": "2022-09-11", "middle": "2023-05-11", "end": "2023-09-11"},
+
+         # Valid - middle same date as end
+         {"name": "E", "start": "2022-09-11", "middle": "2023-05-11", "end": "2023-05-11"},
+     ], ["start", "middle", "end"], 0),
 ])
-def test_checks_valid_chronological_order(source, n_errors) -> None:
+def test_checks_valid_chronological_order(source, field_names, n_errors) -> None:
     """Test the ChronologicalOrder checker"""
     # Construct fake resource
     resource = frictionless.Resource(
@@ -58,10 +94,11 @@ def test_checks_valid_chronological_order(source, n_errors) -> None:
     report = resource.validate(
         checks=[
             plugins.chronological.ChronologicalOrder(
-                field_names=["start", "end"]
+                field_names=field_names
             ),
         ]
     )
 
     # Assert no. of errors is expected
     assert report.stats["errors"] == n_errors
+
