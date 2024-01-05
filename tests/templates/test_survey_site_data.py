7d5bb0fb8354b159957f8f61bdf0dd41ccff34a3
joecrowleygaia <joseph.crowley@gaiaresources.com.au>
2024-01-05 15:21:39 +0800

    added more commentary to the test

--- tests/templates/test_survey_site_data.py
+++ tests/templates/test_survey_site_data.py
@@ -8,16 +8,23 @@ import abis_mapping.templates.survey_site_data.mapping
 
 
 def test_extract_geometry_defaults(mocker: pytest_mock.MockerFixture) -> None:
-    """Test the extract_geometry_defaults method."""
+    """Test the extract_geometry_defaults method.
+
+    Args:
+        mocker (pytest_mock.MockerFixture): The mocker fixture.
+    """
     # Construct a dummy raw data set using only the fields that matter to the method.
     rawh = ["siteID", "footprintWKT", "decimalLongitude", "decimalLatitude"]
     raws = [["site1", "POLYGON((0 0, 0 5, 5 5, 5 0, 0 0))", "", ""],
             ["site2", "POLYGON((0 0, 0 5, 5 5, 5 0, 0 0))", "10.0", "20.0"],
             ["site3", "", "10.0", "20.0"],
             ["site4", "", "", ""]]
+    # Amalgamate into a list of dicts
     all_raw = [{hname: val for hname, val in zip(rawh, ln)} for ln in raws]
 
+    # Get the specific mapper
     mapper = abis_mapping.templates.survey_site_data.mapping.SurveySiteMapper()
+
     # Modify schema to only include the necessary fields for test
     descriptor = {"fields": [
         {"name": "siteID", "type": "string"},
@@ -27,6 +34,7 @@ def test_extract_geometry_defaults(mocker: pytest_mock.MockerFixture) -> None:
     ]}
     mocker.patch.object(base.mapper.ABISMapper, "schema").return_value = descriptor
 
+    # Create raw data csv string
     with io.StringIO() as output:
         csv_writer = csv.DictWriter(output, fieldnames=rawh)
         csv_writer.writeheader()
@@ -42,5 +50,8 @@ def test_extract_geometry_defaults(mocker: pytest_mock.MockerFixture) -> None:
         "site3": "POINT (10 20)",
         "site4": None
     }
+    # Invoke method
     actual = mapper.extract_geometry_defaults(csv_data)
+
+    # Validate
     assert actual == expected
