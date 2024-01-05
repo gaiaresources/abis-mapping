40def416c95a8cb0f8aa21d4650b6bbbdc6a2413
joecrowleygaia <joseph.crowley@gaiaresources.com.au>
2024-01-05 15:14:25 +0800

    implemented site template method to extract map of default geometry to use for each site

--- abis_mapping/templates/survey_site_data/mapping.py
+++ abis_mapping/templates/survey_site_data/mapping.py
@@ -1,5 +1,5 @@
 """Provides ABIS Mapper for `survey_site_data.csv` Template"""
-
+import decimal
 
 # Local
 from abis_mapping import base
@@ -10,6 +10,8 @@ from abis_mapping import vocabs
 # Third-party
 import rdflib
 import frictionless
+import shapely
+import shapely.geometry
 
 # Typing
 from typing import Any, Optional, Iterator
@@ -84,6 +86,56 @@ class SurveySiteMapper(base.mapper.ABISMapper):
 
         # Return validation report
         return report
+
+    def extract_geometry_defaults(
+        self,
+        data: base.types.ReadableType,
+    ) -> dict[str, str | None]:
+        """Constructs a dictionary mapping site id to default WKT.
+
+        The resulting string WKT returned can then be used as the missing
+        geometry for other related templates i.e. the site occurrences
+
+        Args:
+            data (base.types.ReadableType): Raw data to be mapped.
+
+        Returns:
+            dict[str, str | None]: Keys are the site id; values are the
+                appropriate point WKT serialized string or None if no valid
+                found.
+
+        """
+        # Construct schema
+        schema = frictionless.Schema.from_descriptor(self.schema())
+
+        resource = frictionless.Resource(
+            data=data,
+            format="csv",
+            schema=schema,
+        )
+
+        with resource.open() as r:
+            # Create empty dictionary to hold mapping values
+            result = {}
+            for row in r.row_stream:
+                # Extract values
+                site_id: str = row["siteID"]
+                footprint_wkt: shapely.geometry.base.BaseGeometry = row["footprintWKT"]
+                longitude: decimal.Decimal = row["decimalLongitude"]
+                latitude: decimal.Decimal = row["decimalLatitude"]
+
+                # Default to using the footprint wkt
+                if footprint_wkt is not None:
+                    result[site_id] = footprint_wkt.centroid.wkt
+                    continue
+                # If not footprint then we revert to using supplied longitude & latitude
+                if longitude is not None and latitude is not None:
+                    result[site_id] = shapely.Point([longitude, latitude]).wkt
+                    continue
+                # If all these are missing return None for the site ID entry
+                result[site_id] = None
+
+            return result
 
     def apply_mapping(
         self,
---
+++ tests/templates/test_survey_site_data.py
@@ -0,0 +1,46 @@
+"""Tests for the survey_site_data template not common to other templates."""
+import csv
+import io
+from abis_mapping import base
+import pytest_mock
+
+import abis_mapping.templates.survey_site_data.mapping
+
+
+def test_extract_geometry_defaults(mocker: pytest_mock.MockerFixture) -> None:
+    """Test the extract_geometry_defaults method."""
+    # Construct a dummy raw data set using only the fields that matter to the method.
+    rawh = ["siteID", "footprintWKT", "decimalLongitude", "decimalLatitude"]
+    raws = [["site1", "POLYGON((0 0, 0 5, 5 5, 5 0, 0 0))", "", ""],
+            ["site2", "POLYGON((0 0, 0 5, 5 5, 5 0, 0 0))", "10.0", "20.0"],
+            ["site3", "", "10.0", "20.0"],
+            ["site4", "", "", ""]]
+    all_raw = [{hname: val for hname, val in zip(rawh, ln)} for ln in raws]
+
+    mapper = abis_mapping.templates.survey_site_data.mapping.SurveySiteMapper()
+    # Modify schema to only include the necessary fields for test
+    descriptor = {"fields": [
+        {"name": "siteID", "type": "string"},
+        {"name": "footprintWKT", "type": "wkt"},
+        {"name": "decimalLongitude", "type": "number"},
+        {"name": "decimalLatitude", "type": "number"},
+    ]}
+    mocker.patch.object(base.mapper.ABISMapper, "schema").return_value = descriptor
+
+    with io.StringIO() as output:
+        csv_writer = csv.DictWriter(output, fieldnames=rawh)
+        csv_writer.writeheader()
+
+        for row in all_raw:
+            csv_writer.writerow(row)
+
+        csv_data = output.getvalue().encode("utf-8")
+
+    expected = {
+        "site1": "POINT (2.5 2.5)",
+        "site2": "POINT (2.5 2.5)",
+        "site3": "POINT (10 20)",
+        "site4": None
+    }
+    actual = mapper.extract_geometry_defaults(csv_data)
+    assert actual == expected
