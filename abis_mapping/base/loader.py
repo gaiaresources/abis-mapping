f437ef427dc045c0e783727614f2b14d4732cda0
Serge Le Breton <serge.lebreton@gaiaresources.com.au>
2024-04-05 14:55:20 +0800

    BDRSPS-562: the dynamic mapping module loader now only python modules named `mapping.py`

--- abis_mapping/base/loader.py
+++ abis_mapping/base/loader.py
@@ -9,10 +9,10 @@ import pathlib
 # Constants
 PACKAGE_DIRECTORY = pathlib.Path(__file__).parent.parent
 TEMPLATES_DIRECTORY = PACKAGE_DIRECTORY / "templates"
-
+MAPPING_MODULE_NAME = "mapping.py"
 
 # Loop through modules in the directory
-for mapping_module in TEMPLATES_DIRECTORY.glob("**/*.py"):
+for mapping_module in TEMPLATES_DIRECTORY.glob(f"**/{MAPPING_MODULE_NAME}"):
     # Determine Import Path
     relative_path = mapping_module.relative_to(PACKAGE_DIRECTORY.parent)
     import_string = ".".join(relative_path.with_suffix("").parts)
