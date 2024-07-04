7ac3191bd41f0b622600ba8b0c35764cdaa82d8b
joecrowleygaia <joseph.crowley@gaiaresources.com.au>
2024-07-04 16:35:13 +0800

    Created survey occurrence instructions markdown template and associated rendering logic.

--- scripts/generate_instructions.sh
+++ scripts/generate_instructions.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 python docs/instructions.py -o docs/pages/incidental_occurrence_data_v2.md incidental_occurrence_data-v2.0.0.csv
+python docs/instructions.py -o docs/pages/survey_occurrence_data_v1.md survey_occurrence_data-v1.0.0.csv
