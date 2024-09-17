04f022313f3caad80101768b03629539b22ef2cc
Lincoln Puzey <lincoln.puzey@gaiaresources.com.au>
2024-09-17 16:01:36 +0800

    Implement initial sensitivity vocabs
    These will probably be completed later, this initial version is to enable mapping these fields.

---
+++ abis_mapping/vocabs/sensitivity_authority.py
@@ -0,0 +1,25 @@
+"""Provides sensitivity authority vocabulary"""
+
+
+# Third-Party
+import rdflib
+
+# Local
+from abis_mapping import utils
+
+
+# TODO Probably will make this a restricted vocabulary at some point.
+# Vocabulary
+class SensitivityAuthority(utils.vocabs.FlexibleVocabulary):
+    vocab_id = "SENSITIVITY_AUTHORITY"
+    definition = rdflib.Literal("A type of sensitivityAuthority.")
+    base = utils.rdf.uri("bdr-cv/attribute/sensitivityAuthority/")
+    scheme = rdflib.URIRef("http://linked.data.gov.au/def/tern-cv/dd085299-ae86-4371-ae15-61dfa432f924")
+    broader = utils.rdf.uri("bdr-cv/attribute/sensitivityAuthority", utils.namespaces.EXAMPLE)
+    default = None  # No default, omitted if not provided
+    terms = ()
+    publish = False
+
+
+# Register
+utils.vocabs.Vocabulary.register(SensitivityAuthority)
---
+++ abis_mapping/vocabs/sensitivity_category.py
@@ -0,0 +1,25 @@
+"""Provides sensitivity category vocabulary"""
+
+
+# Third-Party
+import rdflib
+
+# Local
+from abis_mapping import utils
+
+
+# TODO complete this vocabulary
+# Vocabulary
+class SensitivityCategory(utils.vocabs.FlexibleVocabulary):
+    vocab_id = "SENSITIVITY_CATEGORY"
+    definition = rdflib.Literal("A type of sensitivityCategory.")
+    base = utils.rdf.uri("bdr-cv/attribute/sensitivityCategory/")
+    scheme = rdflib.URIRef("http://linked.data.gov.au/def/tern-cv/dd085299-ae86-4371-ae15-61dfa432f924")
+    broader = utils.rdf.uri("bdr-cv/attribute/sensitivityCategory", utils.namespaces.EXAMPLE)
+    default = None  # No default, omitted if not provided
+    terms = ()
+    publish = False
+
+
+# Register
+utils.vocabs.Vocabulary.register(SensitivityCategory)
