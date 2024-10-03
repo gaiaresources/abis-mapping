1735826ecd82bff09c88c4fe3b76500e03877ddf
joecrowleygaia <joseph.crowley@gaiaresources.com.au>
2024-10-03 11:04:58 +0800

    added poe the poet and remove unused scripts.

--- scripts/test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -x
-pytest tests --cov=abis_mapping --cov=docs --cov-report=term-missing "${@}"
--- scripts/typecheck.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -x
-mypy tests abis_mapping docs
