ee1db6fe170065731a17967d579e3d8cda3802c2
Lincoln Puzey <lincoln.puzey@gaiaresources.com.au>
2024-10-04 12:21:02 +0800

    BDRSPS-764 Set up context for survey metadata v2 docs

--- docs/contexts/__init__.py
+++ docs/contexts/__init__.py
@@ -6,4 +6,5 @@ from . import incidental_occurrence_data_v2
 from . import incidental_occurrence_data_v3
 from . import survey_occurrence_data
 from . import survey_metadata
+from . import survey_metadata_v2
 from . import survey_site_data
