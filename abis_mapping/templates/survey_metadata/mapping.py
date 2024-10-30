8916617da7151d9bf026a68bd38924e2aa447392
Voon-Li CHUNG <voonli.chung@gaiaresources.com.au>
2024-10-30 17:16:15 +0800

    [BDRSPS-918] First stab at ticket

--- abis_mapping/templates/survey_metadata/mapping.py
+++ abis_mapping/templates/survey_metadata/mapping.py
@@ -502,6 +502,7 @@ class SurveyMetadataMapper(base.mapper.ABISMapper):
         # Extract relevant values
         datum = row["geodeticDatum"]
         sc_geometry = row["spatialCoverageWKT"]
+        spatial_accuracy = row["coordinateUncertaintyInMeters"]
 
         if not (datum and sc_geometry):
             return
@@ -517,6 +518,9 @@ class SurveyMetadataMapper(base.mapper.ABISMapper):
         graph.add((uri, utils.namespaces.GEO.hasGeometry, geometry_node))
         graph.add((geometry_node, a, utils.namespaces.GEO.Geometry))
         graph.add((geometry_node, utils.namespaces.GEO.asWKT, geometry.to_transformed_crs_rdf_literal()))
+
+        if spatial_accuracy is not None:
+            graph.add((geometry_node, utils.namespaces.GEO.hasMetricSpatialAccuracy, spatial_accuracy))
 
         self.add_geometry_supplied_as(
             subj=uri,
