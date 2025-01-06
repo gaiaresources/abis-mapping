94a8f89d1d355a31f5c769a70a8d3bd32d6318af
Lincoln Puzey <lincoln.puzey@gaiaresources.com.au>
2025-01-06 10:37:14 +0800

    Simplify template loading by replacing dynamic imports with regular imports

--- abis_mapping/__init__.py
+++ abis_mapping/__init__.py
@@ -2,5 +2,5 @@
 
 # Local
 from . import plugins  # Ensure plugins are loaded
-from .base import loader  # Dynamically load the template mappers
+from . import templates  # Import templates module to ensure Mappers are registered
 from .base.mapper import register_mapper, get_mapper, registered_ids
