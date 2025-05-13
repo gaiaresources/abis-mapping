896528e2b8f0b699d7c4785e186124692a0cf2eb
Mieke Strong <mieke.strong@gaiaresources.com.au>
2025-05-13 11:36:44 +1000

    modified abis_mapping/vocabs/reproductive_condition.py

--- abis_mapping/vocabs/reproductive_condition.py
+++ abis_mapping/vocabs/reproductive_condition.py
@@ -13,7 +13,7 @@ class ReproductiveCondition(utils.vocabs.FlexibleVocabulary):
     definition = rdflib.Literal("A type of reproductiveCondition.")
     base = "bdr-cv/parameter/reproductiveCondition/"
     proposed_scheme = rdflib.URIRef("http://linked.data.gov.au/def/tern-cv/5699eca7-9ef0-47a6-bcfb-9306e0e2b85e")
-    broader = utils.rdf.uri("bdr-cv/parameter/reproductiveCondition", utils.namespaces.DATASET_BDR)
+    broader = rdflib.URIRef("http://linked.data.gov.au/def/tern-cv/4b2e788d-260b-4cfc-91f4-99e7c2ad2c42")
     default = None  # No default, ommitted if not provided
     terms = ()  # No baseline vocabulary values
     publish = False
