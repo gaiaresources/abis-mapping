8160c291862447b52e26106f2e67e2f4c0868b32
Mieke Strong <mieke.strong@gaiaresources.com.au>
2025-05-13 10:33:39 +1000

    Update vocabulary IRI to use real URIs

--- abis_mapping/vocabs/basis_of_record.py
+++ abis_mapping/vocabs/basis_of_record.py
@@ -10,12 +10,12 @@ from abis_mapping import utils
 # Terms
 HUMAN_OBSERVATION = utils.vocabs.Term(
     labels=("HUMAN OBSERVATION",),
-    iri=utils.rdf.uri("basisOfRecord/HumanObservation", utils.namespaces.EXAMPLE),  # TODO -> Need real URI
+    iri=rdflib.URIRef("http://linked.data.gov.au/def/tern-cv/d8a47898-e5cb-45f5-be87-e4a7dc5d053e"),  # real URI
     description="An output of a human observation.",
 )
 OCCURRENCE = utils.vocabs.Term(
     labels=("OCCURRENCE",),
-    iri=utils.rdf.uri("basisOfRecord/Occurrence", utils.namespaces.EXAMPLE),  # TODO -> Need real URI
+    iri=rdflib.URIRef("http://linked.data.gov.au/def/tern-cv/4008182b-422e-400e-8957-13ca13dbac6b"),  # real URI
     description=(
         "An existence of an Organism (sensu http://rs.tdwg.org/dwc/terms/Organism)"
         " at a particular place at a particular time."
@@ -23,27 +23,27 @@ OCCURRENCE = utils.vocabs.Term(
 )
 PRESERVED_SPECIMEN = utils.vocabs.Term(
     labels=("PRESERVED SPECIMEN",),
-    iri=utils.rdf.uri("basisOfRecord/PreservedSpecimen", utils.namespaces.EXAMPLE),  # TODO -> Need real URI
+    iri=rdflib.URIRef("http://linked.data.gov.au/def/tern-cv/1644f015-216f-4691-bb42-44d3a93bb277"),  # real URI
     description="A specimen that has been preserved.",
 )
 FOSSIL_SPECIMEN = utils.vocabs.Term(
     labels=("FOSSIL SPECIMEN",),
-    iri=utils.rdf.uri("basisOfRecord/FossilSpecimen", utils.namespaces.EXAMPLE),  # TODO -> Need real URI
+    iri=rdflib.URIRef("http://linked.data.gov.au/def/tern-cv/0f876e28-1175-41be-a8e5-ca040444421b"),  # real URI
     description="A preserved specimen that is a fossil.",
 )
 LIVING_SPECIMEN = utils.vocabs.Term(
     labels=("LIVING SPECIMEN",),
-    iri=utils.rdf.uri("basisOfRecord/LivingSpecimen", utils.namespaces.EXAMPLE),  # TODO -> Need real URI
+    iri=rdflib.URIRef("http://linked.data.gov.au/def/tern-cv/d7457b40-94dc-4b18-b7ef-dd3c6744fd02"),  # real URI
     description="A specimen that is alive.",
 )
 MACHINE_OBSERVATION = utils.vocabs.Term(
     labels=("MACHINE OBSERVATION",),
-    iri=utils.rdf.uri("basisOfRecord/MachineObservation", utils.namespaces.EXAMPLE),  # TODO -> Need real URI
+    iri=rdflib.URIRef("http://linked.data.gov.au/def/tern-cv/90cf285e-ed6d-41f6-aaea-8331e9bd07df"),  # real URI
     description="An output of a machine observation process.",
 )
 MATERIAL_SAMPLE = utils.vocabs.Term(
     labels=("MATERIAL SAMPLE",),
-    iri=utils.rdf.uri("basisOfRecord/MaterialSample", utils.namespaces.EXAMPLE),  # TODO -> Need real URI
+    iri=rdflib.URIRef("http://linked.data.gov.au/def/tern-cv/6ad1354b-3131-4663-9c78-45c015d42a35"),  # real URI
     description=(
         "A physical result of a sampling (or subsampling) event. In biological collections, the "
         "material sample is typically collected, and either preserved or destructively processed."
@@ -57,7 +57,7 @@ class BasisOfRecord(utils.vocabs.FlexibleVocabulary):
     definition = rdflib.Literal("A type of basisOfRecord.")
     base = "bdr-cv/attribute/basisOfRecord/"
     proposed_scheme = rdflib.URIRef("http://linked.data.gov.au/def/tern-cv/dd085299-ae86-4371-ae15-61dfa432f924")
-    broader = utils.rdf.uri("bdr-cv/attribute/basisOfRecord", utils.namespaces.DATASET_BDR)
+    broader = iri=rdflib.URIRef("http://linked.data.gov.au/def/tern-cv/633119a9-6c8a-46ae-a78a-23a4fd371eb2")
     default = None  # No default, omitted if not provided
     terms = (
         HUMAN_OBSERVATION,
