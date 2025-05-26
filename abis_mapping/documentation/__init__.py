cc9617e376bfee8a09c58184fc43ecd2b0576ee5
Lincoln Puzey <lincoln.puzey@gaiaresources.com.au>
2025-05-26 10:18:54 +0800

    Rename dataset properties page, which also makes the url change

--- abis_mapping/documentation/__init__.py
+++ abis_mapping/documentation/__init__.py
@@ -10,12 +10,12 @@ import pydantic
 from abis_mapping import settings
 
 
-def get_submission_properties_url() -> str:
-    """Get a url to the submission properties page of the documentation."""
+def get_dataset_properties_url() -> str:
+    """Get a url to the dataset properties page of the documentation."""
     # Create url to the docs
     url = urllib.parse.urljoin(
         base=settings.SETTINGS.INSTRUCTIONS_BASE_URL,
-        url="/".join([settings.SETTINGS.INSTRUCTIONS_VERSION, "submission_properties"]),
+        url="/".join([settings.SETTINGS.INSTRUCTIONS_VERSION, "dataset_properties"]),
     )
     # Check it is valid url
     pydantic.AnyUrl(url)
